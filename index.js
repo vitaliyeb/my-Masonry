9bac7f9cb298b09a909f34861687747bba97b87a
vitaliyeb <vlvlv.vovov@bk.ru>
2020-09-16 12:03:23 +0300

    тестирую баги

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ window.addEventListener('load', function (){
                 });
             }
             sortCurrentRow(currentRow, row);
+            setGridParams(map[row], row);
         }
 
 
@@ -48,64 +49,11 @@ window.addEventListener('load', function (){
             });
         }
 
-        // for (let row = 0; row < Math.ceil(allItems.length / countColomn); row++){
-        //     currentRowSort= [];
-        //     map.push([]);
-        //     for (let col = 0; col < countColomn; col++ ){
-        //                 $el: false,
-        //                 rowBusy: 0
-        //             });
-        //             continue;
-        //         };
-        //         currentRowSort.push({
-        //             $el: $(el),
-        //             rowBusy: Math.ceil($(el).height() / fullRowSize)
-        //         });
-        //     }
-        //     if (currentRowSort.length) createRow(row);
-            
-           
-        //     setGridParams(map[row], row);
-            
-        // };
-
-
-
-        function createRow(row) {
-            let predRow = map[row-1].map((el, ind)=>({bussyAll: el.bussyAll, ind}));
-            
-            currentRowSort.sort((e1, e2)=> e2.rowBusy - e1.rowBusy);
-
-            predRow.sort((el1, el2)=>el1.bussyAll - el2.bussyAll);
-
-            currentRowSort.map((el, i)=>{
-                let predElem = predRow[i];
-
-                map[row][predElem['ind']] = 
-                {
-                    el: el.$el[0],
-                    rowBusy: el.rowBusy,
-                    bussyAll: predElem.bussyAll + el.rowBusy
-                }
-            });
-        }
-
-        function createFirstRow(row, col, el){
-            let $el = $(el);
-            let busyRow = Math.ceil($el.height() / fullRowSize);
-
-            map[row][col] = {
-                el,
-                rowStart: 1,
-                bussyAll: busyRow
-            }
-        }
-
         function setGridParams(row, rowInd) {
-            row.map((item, ind)=>{
-                if(!item.el) return;
-                item.el.style.gridRow = `${item.rowStart || map[rowInd - 1][ind].bussyAll + 1} / ${item.bussyAll}`;
-                item.el.style.gridColumn = `${ind+1} / ${ind+2}`;
+            row.map(({el, rowBusy, busyAll}, ind)=>{
+                if(!el) return;
+                el.style.gridRow = `${busyAll - rowBusy + 1} / ${busyAll}`;
+                el.style.gridColumn = `${ind+1} / ${ind+2}`;
                
             });
         }
