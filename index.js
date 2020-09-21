9600aff43ca9585e1eef248d726d513dd044ff95
vitaliyeb <vlvlv.vovov@bk.ru>
2020-09-21 20:15:18 +0300

    сделал вид как у мини библиотеки , с кучей ошибок в реализации

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-window.addEventListener('load', function (){
-    let masonryWrapper = document.getElementById('myMasonry');
-    if(!masonryWrapper) return ;
 
-    let allItems = masonryWrapper.children,
-        rowSize = 5,
-        cssDatamasonryWrapper = getComputedStyle(masonryWrapper),
-        rowGap = parseInt(cssDatamasonryWrapper.rowGap) || 0,
-        columnGap = parseInt(cssDatamasonryWrapper.columnGap),
-        itemWidth = Number(allItems[0].offsetWidth ),
-        wrapperWidth =
-         Number(masonryWrapper.offsetWidth - parseInt(cssDatamasonryWrapper.paddingLeft) - parseInt(cssDatamasonryWrapper.paddingRight)),
-        countColomn = Math.ceil(wrapperWidth / (itemWidth+columnGap)),
-        fullRowSize = rowSize + rowGap,
-        rowCount = Math.ceil(masonryWrapper.offsetHeight / fullRowSize),
-        map = [];
+(function(){
 
-        for (let row = 0; row < Math.ceil(allItems.length / countColomn); row++){
-            let currentRow = [];
-            for (let col = 0; col < countColomn; col++ ){
-                let el = allItems[row * countColomn + col];
-                let rowBusy = el ? Math.ceil(el.offsetHeight / fullRowSize) + 1 : -1;
-                currentRow.push({
-                    el,
-                    rowBusy,
-                    busyAll: row === 0 ? rowBusy : undefined
-                });
+        function init(masonryWrapper) {
+            let cssDataMW = getComputedStyle(masonryWrapper),
+                allItems = masonryWrapper.children,
+                rowSize = 10,
+                rowGap = parseInt(cssDataMW.rowGap) || 0,
+                columnGap = parseInt(cssDataMW.columnGap) || 0,
+                itemWidth = Number(allItems[0].offsetWidth ),
+                wrapperWidth = Number(masonryWrapper.offsetWidth - parseInt(cssDataMW.paddingLeft) - parseInt(cssDataMW.paddingRight)),
+                countColomn = Math.ceil(wrapperWidth / (itemWidth+columnGap)),
+                fullRowSize = rowSize + rowGap,
+                rowCount = Math.ceil(masonryWrapper.offsetHeight / fullRowSize),
+                map = [];
+
+            for (let row = 0; row < Math.ceil(allItems.length / countColomn); row++){
+                let currentRow = [];
+                for (let col = 0; col < countColomn; col++ ){
+                    let el = allItems[row * countColomn + col];
+                    let rowBusy = el ? Math.ceil(el.offsetHeight / fullRowSize) + 1 : -1;
+                    currentRow.push({
+                        el,
+                        rowBusy,
+                        busyAll: row === 0 ? rowBusy : undefined
+                    });
+                }
+                sortCurrentRow(map, currentRow, row);
+                setGridParams(map[row], row);
             }
-            sortCurrentRow(currentRow, row);
-            setGridParams(map[row], row);
+    
+            let maxRowCount = Math.max(...map[map.length-1].map(item=>item.busyAll));
+            masonryWrapper.style.gridTemplateRows =  `repeat(${ maxRowCount }, ${rowSize}px)`;
+            masonryWrapper.style.alignItems = 'stretch';
         }
-        setLastParams();
 
-        function sortCurrentRow(currentRow, rowIndex) {
+        function sortCurrentRow(map, currentRow, rowIndex) {
             if(rowIndex === 0) return map.push(currentRow);
             map[rowIndex] = [];
             
@@ -60,13 +63,17 @@ window.addEventListener('load', function (){
             });
         }
 
-        function setLastParams() {
-            let maxRowCount = Math.max(...map[map.length-1].map(item=>item.busyAll));
-            masonryWrapper.style.gridTemplateRows =  `repeat(${ maxRowCount }, ${rowSize}px)`;
-            masonryWrapper.style.alignItems = 'stretch';
+        function setVariable(){
+
         }
 
-     
-    
+        window.myMasonry = function(masonryWrapper, onUpdateResize ){  
+            
+            if(onUpdateResize) window.addEventListener('resize', init);
+            init(masonryWrapper);
+        }
 
-});
+})();
+
+
+myMasonry(document.getElementById('myMasonry'));
